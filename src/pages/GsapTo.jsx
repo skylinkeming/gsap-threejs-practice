687e35c3c590d7c3cf232631780649963837fce1
stevelin <germinglin@yahoo.com.tw>
2024-09-10 13:53:16 +0800

    1st

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,6 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
-
+  useGSAP(() => {
+    gsap.to("#blue-box", {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotate: 360,
+      duration: 2,
+      ease:"bounce-in"
+    });
+  }, []);
   return (
     <main>
       <h1>GsapTo</h1>
